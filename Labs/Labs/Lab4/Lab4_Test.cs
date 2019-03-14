b666e80dc7d15f5b0466cdff82a2c02d23b09e89
ryderhsu <ryder_hsu@mayohr.com>
2019-03-14 16:18:34 +0800

    加入 Lab4

---
+++ Labs/Labs/Lab4/Lab4_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs.Lab4
+{
+    public class Lab4_Test
+    {
+        public void Test_Calculate()
+        {
+            //// Arrange
+            var expected = 100;
+            var calculate = new Calculator();
+
+            //// Act
+            var actual = calculate.Add(1, 2);
+
+            //// Assert
+        }
+    }
+}
---
+++ Labs/Labs/Lab4/README.md
@@ -0,0 +1,1 @@
+#Lab4 練習 Assertion
